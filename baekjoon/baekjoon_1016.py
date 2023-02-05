49e536bfab15aad06de743d978e20f5576d18ebe
Geon Ryu <65989284+Pr0d0@users.noreply.github.com>
2023-02-05 21:46:55 +0900

    Use sieve of Eratostenes
    apply the algorithm except only the squares in the from minimum to maximum number

--- baekjoon/baekjoon_1016.py
+++ baekjoon/baekjoon_1016.py
@@ -1,4 +1,5 @@
 '''
+
 # 1. 시간 복잡도 O(n) - 일일이 지우기  [시간 초과]
 
 from sys import stdin as s
@@ -21,36 +22,28 @@ print(cnt)
 print(mx-mn+1-cnt)
 '''
 
-# 2. 에라토스테네스의 체 (하나씩 지워나가기)
+# 2. 에라토스테네스의 체 알고리즘 사용(하나씩 지워나가기)
+# 에라토스테네스의 체 초기화: n개 요소에 False 설정(소수로 간주)
+# 어짜피 제곱수 4의 배수를 지우면 16의 배수도 마찬가지로 지워지는 개념
+# 빼야하는 제곱수의 최솟값을 min보다 작은 제곱수 중 가장 큰 제곱수부터 mx전까지의 제곱수의 배수를 제외한다.
+# 메모리 초과를 방지할 수 있음
 
 from sys import stdin as s
 from math import sqrt
 
 
-def prime_list(mx, lst):
-    # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
-    sieve = [True] * mx
-
-    for i in range(2, round(sqrt(mx)) + 1):
-        if sieve[i]:           # i가 소수인 경우
-            for j in range(i+i, mx, i): # i이후 i의 배수 들을 False 판정
-                sieve[j] = False
-
-    # 나눠야 하는 제곱수 목록 산출
-    sqaureNumList = [i**2 for i in range(2, mx) if sieve[i] == True]
-    for i in sqaureNumList:
-        for j in lst:
-            if j % i == 0:
-                lst.remove(j)
-    return lst
-
-
 mn, mx = map(int, s.readline().split())
 inputList = [i for i in range(mn, mx+1)]
+cnt = mx-mn+1
 
 
-if mn == 1:  # mn이 1일 경우에 1을 제외하고 함수 계산 후 다시 1이 포함된 제곱ㄴㄴ수 리스트의 개수 출력
-    inputList.remove(1)
-    print(len(prime_list(mx, inputList))+1)
-else:
-    print(len(prime_list(mx, inputList)))
+sieve = [False] * cnt
+
+for i in range(2, round(sqrt(mx)) + 1):
+    square = i ** 2
+    for j in range((((mn - 1) // square) + 1) * square, mx+1, square):  #
+        if not sieve[j-mn]:
+            sieve[j - mn] = True # 한 번 제외된 제곱수의 배수들을 다시 계산하지 않게끔 true로 설정
+            cnt -= 1
+
+print(cnt)
