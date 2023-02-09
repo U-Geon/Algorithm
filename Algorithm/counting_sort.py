4164a38ce79f58509a0a69346d81e8395d20fa86
Geon Ryu <65989284+Pr0d0@users.noreply.github.com>
2023-02-09 21:23:42 +0900

    Add other method

--- Algorithm/counting_sort.py
+++ Algorithm/counting_sort.py
@@ -1,4 +1,16 @@
-def counting_sort(arr):
+def counting_sort_no1(arr):
+    count = [0] * (max(arr) + 1)  # 0~배열의 최댓값까지 범위의 0 리스트
+
+    for num in arr: # count 배열의 인덱스를 값으로 갖는 arr의 요소의 갯수를 대입
+        count[num] += 1
+
+    result = []
+    for i in range(max(arr)+1): # 오름차순으로 각 개수대로 출력
+        for j in range(count[i]):
+            result.append(i)
+
+
+def counting_sort_no2(arr):
     count = [0] * (max(arr) + 1)
 
     for num in arr:
