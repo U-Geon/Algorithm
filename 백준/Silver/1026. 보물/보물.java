90c1189b1f29fef6ebe36a0760db79e7a286d94e
Geon Ryu <65989284+U-Geon@users.noreply.github.com>
2024-07-12 01:14:43 +0900

    [Silver IV] Title: 보물, Time: 108 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        Integer[] b = new Integer[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(a);
+        Arrays.sort(b, Comparator.reverseOrder());
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            count += a[i] * b[i];
+        }
+        System.out.println(count);
+    }
+}
