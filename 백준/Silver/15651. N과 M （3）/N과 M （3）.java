358e0b22f4d76a984cccb6a6e5295e7eed22f471
Geon Ryu <65989284+U-Geon@users.noreply.github.com>
2024-08-28 17:38:57 +0900

    [Silver III] Title: N과 M (3), Time: 516 ms, Memory: 160972 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedWriter bw;
+    static int[] arr;
+
+    static void solution(int index, int n, int m) throws IOException {
+        if(m == index) {
+            for (int i = 0; i < m; i++) {
+                bw.write(arr[i] + " ");
+            }
+            bw.newLine(); // 줄 바꿈
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[index] = i + 1;
+            solution(index + 1, n, m);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+
+        solution(0, n, m);
+
+        bw.flush(); // 출력 버퍼를 비워 실제로 출력
+        bw.close(); // BufferedWriter를 닫아 자원을 해제
+    }
+}
