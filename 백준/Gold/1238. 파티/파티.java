bc8043155a81c8eef71b6f86496ac6bc8279d6b5
Geon Ryu <65989284+U-Geon@users.noreply.github.com>
2024-12-30 17:35:44 +0900

    [Gold III] Title: 파티, Time: 176 ms, Memory: 18044 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,7 +1,53 @@
 import java.io.*;
 import java.util.*;
 
+class Student implements Comparable<Student> {
+    int next, weight;
+
+    public Student(int next, int weight) {
+        this.next = next;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        if(this.weight != o.weight) {
+            return Integer.compare(this.weight, o.weight);
+        }
+        return 0;
+    }
+}
+
 public class Main {
+
+    static int[] dijkstra(ArrayList<ArrayList<Student>> graph, Student student) {
+        int[] dist = new int[graph.size()];
+        Arrays.fill(dist, 100000000);
+        boolean[] visited = new boolean[graph.size()];
+
+        PriorityQueue<Student> pq = new PriorityQueue<>();
+        pq.add(student);
+        dist[student.next] = 0;
+
+        while(!pq.isEmpty()) {
+            Student curr = pq.poll();
+            visited[curr.next] = true;
+
+            ArrayList<Student> students = graph.get(curr.next);
+
+            for (int i = 0; i < students.size(); i++) {
+                Student neighbor = students.get(i);
+
+                if(!visited[neighbor.next] && dist[neighbor.next] > dist[curr.next] + neighbor.weight) {
+                    dist[neighbor.next] = dist[curr.next] + neighbor.weight;
+                    pq.add(new Student(neighbor.next, dist[neighbor.next])); // 거리가 갱신된 노드를 삽입해야 함.
+                }
+            }
+        }
+
+        return dist;
+    }
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,14 +57,12 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
         int x = Integer.parseInt(st.nextToken());
 
-        int[][] distance = new int[n+1][n+1];
+        ArrayList<ArrayList<Student>> graph = new ArrayList<>();
+        ArrayList<ArrayList<Student>> reversedGraph = new ArrayList<>();
 
-        for (int i = 1; i <= n; i++) {
-            Arrays.fill(distance[i], 1000000); // 엄청 큰 값, Integer.MAX_VALUE는 제외;
-        }
-
-        for (int i = 1; i <= n; i++) {
-            distance[i][i] = 0;
+        for (int i = 0; i < n + 1; i++) {
+            graph.add(new ArrayList<>());
+            reversedGraph.add(new ArrayList<>());
         }
 
         for (int i = 0; i < m; i++) {
@@ -27,25 +71,18 @@ public class Main {
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
             
-            distance[a][b] = c;
+            graph.get(a).add(new Student(b, c));
+            reversedGraph.get(b).add(new Student(a, c));
         }
 
-        // 폴로이드-워셜 알고리즘
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                for (int k = 1; k <= n; k++) {
-                    distance[j][k] = Math.min(distance[j][k], distance[j][i] + distance[i][k]);
-                }
-            }
+        int[] xToHomeDistance = dijkstra(graph, new Student(x, 0));
+        int[] homeToXDistance = dijkstra(reversedGraph, new Student(x, 0));
+        int maxValue = -1;
+
+        for (int i = 1; i < n + 1; i++) {
+            maxValue = Math.max(maxValue, xToHomeDistance[i] + homeToXDistance[i]);
         }
 
-        int max = -1;
-
-        for (int i = 1; i <= n; i++) {
-            if(x == i) continue;
-            max = Math.max(max, distance[x][i] + distance[i][x]);
-        }
-
-        System.out.println(max);
+        System.out.println(maxValue);
     }
 }
