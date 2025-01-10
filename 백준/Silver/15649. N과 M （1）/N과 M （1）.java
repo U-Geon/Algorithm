afe4332152e066f6ab5d0b9c9650cd4afd22f02d
Geon Ryu <65989284+U-Geon@users.noreply.github.com>
2025-01-10 10:41:41 +0900

    [Silver III] Title: N과 M (1), Time: 1696 ms, Memory: 56132 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,36 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static ArrayList<Integer> arr = new ArrayList<>();
-    static void solution(int n, int m) {
-        if(arr.size() == m) {
-            for (Integer i : arr) {
+
+    static int[] permutation;
+    static boolean[] visited;
+
+    static void solution(int n, int m, int index) {
+
+        if(index == m) {
+            for (int i : permutation) {
                 System.out.print(i + " ");
             }
             System.out.println();
             return ;
         }
-        for (int i = 1; i < n+1; i++) {
-            if(!arr.contains(i)) {
-                arr.add(i);
-                solution(n, m);
-                arr.remove(arr.size()-1);
+
+        for (int i = 0; i < n; i++) {
+            // 만약 해당 노드를 방문하지 않았다면?
+            if (!visited[i]) {
+                visited[i] = true;
+                permutation[index] = i + 1;
+                solution(n, m, index + 1);
+
+                // 자식 노드 방문이 끝나고 돌아오면 방문 노드를 방문하지 않은 상태로 변경
+                visited[i] = false;
             }
         }
     }
 
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        solution(n,m);
+        permutation = new int[m];
+        visited = new boolean[n];
+
+        solution(n, m, 0);
     }
 }
