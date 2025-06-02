a24d0e95f1d74f7e9391fff968dfed60d027d3df
Geon Ryu <65989284+U-Geon@users.noreply.github.com>
2025-06-02 16:14:40 +0900

    [level 3] Title: 정수 삼각형, Time: 11.54 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -2,17 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] triangle) {
+        // 거쳐간 숫자의 합이 가장 큰 경우
+        // working backward
+        // 맨 마지막 도착 시 맨 왼쪽 / 맨 오른쪽 / 가운데
         
         int length = triangle.length;
         
-        if(length == 1) return triangle[0][0];
+        int[][] dp = new int[length][];
+        for(int i = 0; i < length; i++) {
+            dp[i] = new int[i+1];
+        }
         
-        for(int i = length - 2; i >= 0; i--) {
-            for(int j = 0; j < triangle[i].length; j++) {
-                triangle[i][j] = Math.max(triangle[i+1][j] + triangle[i][j], triangle[i+1][j+1] + triangle[i][j]);
+        dp[0][0] = triangle[0][0];
+        
+        for(int i=1; i < length; i++) {
+            for(int j=0; j < triangle[i].length; j++) {
+                if(j == 0) {
+                    dp[i][j] = dp[i-1][j] + triangle[i][j];
+                } else if(j == triangle[i].length - 1) {
+                    dp[i][j] = dp[i-1][j-1] + triangle[i][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
+                }
             }
         }
         
-        return triangle[0][0];
+        return Arrays.stream(dp[length-1]).max().getAsInt();
     }
 }
